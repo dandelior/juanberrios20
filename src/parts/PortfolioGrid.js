cede7f7525c076dacef2691b220b9edfba9f52c3
dandelior <jberrioscifuentes@gmail.com>
2020-10-04 23:50:27 -0300

    change work covers

--- src/parts/PortfolioGrid.js
+++ src/parts/PortfolioGrid.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../assets/sass/portfolioGrid.sass';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { worksProvider } from '../WorksProvider';
 
 export class PortfolioGrid extends React.Component {
@@ -14,12 +14,7 @@ export class PortfolioGrid extends React.Component {
                     {works.map((work) => (
                         <div className="item">
                             <Link to={`work/${work.link}`} className="img" style={{backgroundImage: `url('${process.env.PUBLIC_URL + '/img/' + work.imgLink + '.png'}')` }}></Link>
-                            {/* <a href={work.link} className="img" style={{backgroundImage: `url('${process.env.PUBLIC_URL + '/img/saga-cover.png'}')` }}>
-                            </a> */}
                             <div className="data">
-                                {/* <a href={work.link}>
-                                    <h4>{work.title}</h4>
-                                </a> */}
                                 <Link to={`work/${work.link}`}>
                                     <h4>{work.title}</h4>
                                 </Link>
