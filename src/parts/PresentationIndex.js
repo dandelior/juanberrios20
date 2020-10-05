14ea515fe6c06f234e5580da9965db30c7e6e982
dandelior <jberrioscifuentes@gmail.com>
2020-10-05 18:14:24 -0300

    link import fix

--- src/parts/PresentationIndex.js
+++ src/parts/PresentationIndex.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../assets/sass/presentation.sass';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 // import { PiioElement } from 'piio-images-react'
 
 export class PresentationIndex extends React.Component {
